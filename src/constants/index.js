8cbbe508c31a81a5ca096c600818d08382c5835a
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-12 23:48:45 +0530

    Routes to be handled in the app

---
+++ src/constants/index.js
@@ -0,0 +1,11 @@
+// export * from './Routes';
+
+const Routes = {
+    LANDING : '/',
+    SIGN_UP : '/signup',
+    SIGN_IN : '/signin',
+    CHAT : '/chat',
+    RESETPASSWORD : '/resetPassword'
+}
+
+export default Routes;
