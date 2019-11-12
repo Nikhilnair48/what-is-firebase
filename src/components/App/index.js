a49df047a1e8f67d255161d39c2ac8ff2d4877eb
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-12 23:51:57 +0530

    Tying all the components together in App

---
+++ src/components/App/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from '../Nav/Navigation';
+import Routes from '../../constants';
+import { Route } from 'react-router-dom';
+
+import Chat from '../Chat';
+import Landing from '../Landing';
+import ResetPassword from '../ResetPassword';
+import SignIn from '../SignIn';
+import SignUp from '../SignUp';
+
+import './App.css';
+
+export default class App extends React.Component {
+    render() {
+        let appProps = this.props;
+        return (
+            <Router>
+                <div>
+                    <Navigation />
+                    
+                    <h1 />
+
+                    <Route exact path={Routes.LANDING} component={Landing} />
+                    <Route path={Routes.CHAT} component={Chat} />
+                    <Route path={Routes.SIGN_IN} component={SignIn} />
+                    <Route path={Routes.SIGN_UP} component={SignUp} />
+                    <Route path={Routes.RESETPASSWORD} component={ResetPassword} />
+                </div>
+            </Router>
+        )
+    }
+}
