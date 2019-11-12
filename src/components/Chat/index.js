5a7290c31e350ddaf03e92bf0a8a8ba827842244
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-12 23:50:11 +0530

    Sample stubs for Chat, Landing, ResetPassword and SignIn

---
+++ src/components/Chat/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Chat extends React.Component {
+  
+  render() {
+    return (
+      <div>
+        <h1>Chat</h1>
+      </div>
+    )
+  }
+  
+}
---
+++ src/components/ResetPassword/index.js
@@ -0,0 +1,13 @@
+// export * from './ResetPassword';
+import React from 'react';
+
+export default class ResetPassword extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>ResetPassword</h1>
+      </div>
+    )
+  }
+}
