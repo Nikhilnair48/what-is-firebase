ee871d6de6e0cea24b7661c4bd1ca8cfaadd920f
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-12 23:49:35 +0530

    Hyperlinks to navigate between routes. Currently used for testing

---
+++ src/components/Nav/index.js
@@ -0,0 +1,27 @@
+import Routes from '../../constants/';
+
+export default class Navigation extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>
+                        <Link to={Routes.SIGN_IN}>Sign In</Link>
+                    </li>
+                    <li>
+                        <Link to={Routes.SIGN_UP}>Sign Up</Link>
+                    </li>
+                    <li>
+                        <Link to={Routes.LANDING}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={Routes.CHAT}>Chat</Link>
+                    </li>
+                    <li>
+                        <Link to={Routes.RESETPASSWORD}>ResetPassword</Link>
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
