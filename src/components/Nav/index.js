1007bc2840f7ee27f5e2f6e8f9941b561288e4d7
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-13 12:54:46 +0530

    Dynamic navigation based on user auth state; depends on prop authUser

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,31 +2,45 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Routes from '../../constants/';
 
-export default class Navigation extends React.Component {
-    render() {
-        return (
-            <div>
-                <ul>
-                    <li>
-                        <Link to={Routes.SIGN_IN}>Sign In</Link>
-                    </li>
-                    <li>
-                        <Link to={Routes.SIGN_UP}>Sign Up</Link>
-                    </li>
-                    <li>
-                        <Link to={Routes.SIGN_OUT}>Sign Out</Link>
-                    </li>
-                    <li>
-                        <Link to={Routes.LANDING}>Home</Link>
-                    </li>
-                    <li>
-                        <Link to={Routes.CHAT}>Chat</Link>
-                    </li>
-                    <li>
-                        <Link to={Routes.RESETPASSWORD}>ResetPassword</Link>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const Navigation = ({ authUser }) => (
+    <div>
+        {
+            authUser ? <NavigationAuth /> : <NavigationNonAuth />
+        }
+    </div>
+);
+
+const NavigationAuth = () => (
+    <div>
+        <ul>
+            <li>
+                <Link to={Routes.LANDING}>Home</Link>
+            </li>
+            <li>
+                <Link to={Routes.CHAT}>Chat</Link>
+            </li>
+            <li>
+                <Link to={Routes.SIGN_OUT}>Sign Out</Link>
+            </li>
+            <li>
+                <Link to={Routes.RESETPASSWORD}>ResetPassword</Link>
+            </li>
+        </ul>
+    </div>
+);
+
+const NavigationNonAuth = () => (
+    <div>
+        <li>
+            <Link to={Routes.LANDING}>Home</Link>
+        </li>
+        <li>
+            <Link to={Routes.SIGN_IN}>Sign In</Link>
+        </li>
+        <li>
+            <Link to={Routes.SIGN_UP}>Sign Up</Link>
+        </li>
+    </div>
+);
+
+export default Navigation;
