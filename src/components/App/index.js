9df4ecfa763e6b08b4a7d4468888ce29c86cb605
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-13 10:34:56 +0530

    Minor fix: imports

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navigation from '../Nav/Navigation';
+import Navigation from '../Nav';
 import Routes from '../../constants';
 import { Route } from 'react-router-dom';
 
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Routes from '../../constants/';
 
 export default class Navigation extends React.Component {
