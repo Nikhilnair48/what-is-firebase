be88b912d10981a814fc4cbd0e00ebb314805680
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-13 12:16:09 +0530

    Functional implementation of SignOut - complete. User is redirected to Home page (aka Landign) once signed out.

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import { withFirebase } from '../Firebase'; 
+import { withRouter } from 'react-router-dom';
+import './SignOut.css';
+import Routes from '../../constants';
 
-class SignOut extends React.Component {
+const SignOut = () => (
+    <SignOutService />
+  );
+
+class SignOutPage extends React.Component {
+    
+    constructor(props) {
+        super(props);
+
+        this.signOut = this.signOut.bind(this);
+    }
+
+    signOut() {
+        this.props.firebase
+        .doSignOut()
+        .then(response => {
+            console.log(response);
+            this.props.history.push(Routes.LANDING);
+        },
+        error => {
+            console.log(error);
+        });
+    }
+    
     render() {
         return (
             <div>
                 <h1>SignOut</h1>
+                <div>Are you sure?</div>
+                <button className="btn btn-primary" onClick={this.signOut}>Yes</button>
             </div>
-        )
+        );
     }
 }
+
+const SignOutService = withRouter(withFirebase(SignOutPage));
+
+export default SignOut;
+
+export { SignOutService };
