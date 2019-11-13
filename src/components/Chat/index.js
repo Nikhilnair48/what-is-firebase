4e7c8540e139d96de831edc4cd8d66f5cd790380
Nikhilnair48 <nikhil.nair48@gmail.com>
2019-11-13 15:49:05 +0530

    Realtime fetch for chatroom - complete.

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,13 +1,66 @@
 import React from 'react';
+import { withFirebase } from '../Firebase';
+import './Chat.css';
+class Chat extends React.Component {
 
-export default class Chat extends React.Component {
-  
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messages: [],
+    }
+    this.renderMessages = this.renderMessages.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.firebase.chatroom().on('value', snapshot => {
+      let newMessages = [];
+      snapshot.forEach(element => {
+        newMessages.push(element.val());
+      });
+      this.setState({
+        messages: newMessages
+      }); 
+    });
+  }
+
+  renderMessages() {
+      
+      if(this.state.messages.length > 0) {
+        return (
+          this.state.messages.map(message => {
+            return (
+              <div className="message" key={message.messageId}>
+                <img className={this.props.firebase.auth.currentUser.email === message.email ? "right" : ""} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaGFhx5NYzOoOXr4Vys6r1yHNsTiW6WYRfiK9bqzicAv1YqVn9&s" alt="Avatar" />
+                <p>{message.messageText}</p>
+                <span className={this.props.firebase.auth.currentUser.email === message.email ? "time-left" : "time-right"}>{ new Date(message.messageTimestamp * 1000).toUTCString().slice(-11, -4) }</span>
+              </div>
+            )
+          })
+        );
+      } else {
+        return (
+          <div>Coming!</div>
+        )
+      }
+  }
+
   render() {
     return (
       <div>
         <h1>Chat</h1>
+        <div className="chat-container">
+          <div className="message-container">
+            {this.renderMessages()}
+          </div>
+          <div className="user-input">
+            <input className="form-control" type="text" placeholder="" />
+          </div>
+        </div>
       </div>
     )
   }
   
 }
+
+export default withFirebase(Chat);
